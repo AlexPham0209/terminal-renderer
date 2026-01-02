3a8c6132852c8288a0bf191266655dc9957c14e1
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2026-01-02 17:08:32 -0500

    Added more characters to gradient

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,11 @@ pub use crate::vector::vector2::Vector2;
 pub use crate::vector::vector4::Vector4;
 use crate::{
     matrix::{
-        matrix::Matrix, matrix3::Matrix3, matrix4::Matrix4, rotation::{Angle, Rotation}, scale::Scale
+        matrix::Matrix,
+        matrix3::Matrix3,
+        matrix4::Matrix4,
+        rotation::{Angle, Rotation},
+        scale::Scale,
     },
     vector::{vector::Vector, vector3::Vector3},
 };
@@ -54,7 +58,6 @@ struct Transform {
 
     position: Vector3,
     scale: f32,
-    value: char
 }
 
 // Make sure that points are in counter-clockwise order
@@ -94,8 +97,14 @@ fn to_screen_coordinates(vec: Vector3) -> Vector3 {
     )
 }
 
-fn rasterize_triangle(t: Triangle, grid: &mut Grid<char>, depth_buffer: &mut Grid<f32>, value: char)  {
-    let Triangle {a, b, c} = t;
+fn rasterize_triangle(
+    t: Triangle,
+    grid: &mut Grid<char>,
+    depth_buffer: &mut Grid<f32>,
+    normal: Vector3,
+    light: Vector3
+) {
+    let Triangle { a, b, c } = t;
 
     // Skip if any of the points are behind the camera
     if a.z < 0.0 || b.z < 0.0 || c.z < 0.0 || a.z > 1.0 || b.z > 1.0 || c.z > 1.0 {
@@ -104,7 +113,7 @@ fn rasterize_triangle(t: Triangle, grid: &mut Grid<char>, depth_buffer: &mut Gri
 
     let (min_x, min_y, max_x, max_y) = t.get_bounding_box();
     let abc = edge_function(a, b, c);
-
+    let gradient = "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
     // Iterating through every pixel/point inside of triangle's bounding box
     for y in min_y..max_y {
         for x in min_x..max_x {
@@ -119,18 +128,22 @@ fn rasterize_triangle(t: Triangle, grid: &mut Grid<char>, depth_buffer: &mut Gri
                 continue;
             }
 
-            let depths = 1.0 / Vector3::new(
-                a.z, b.z, c.z
-            );
-
             // Barycentric coordinates
-            let weights = Vector3::new(
-                abp / abc, bcp / abc, cap / abc
-            );
+            let weights = Vector3::new(abp / abc, bcp / abc, cap / abc);
+
+            let depths = 1.0 / Vector3::new(a.z, b.z, c.z);
+            let depth = 1.0 / depths.dot(weights);
+
+            // Calculating light value
+            let l = (light - normal).normalize();
+            let value = (normal.dot(l) + 1.0) / 2.0;
+            let value = f32::round(value * ((gradient.len() - 1) as f32)) as usize;
+            let value: char = gradient.as_bytes()[value] as char;
             
             // Calculates the depth and uses it to determine whether current pixel is has lowest depth
-            let depth = 1.0 / depths.dot(weights);
-            if let Some(prev) = depth_buffer.get(x, y) && depth >= *prev {
+            if let Some(prev) = depth_buffer.get(x, y)
+                && depth >= *prev
+            {
                 continue;
             }
 
@@ -138,7 +151,7 @@ fn rasterize_triangle(t: Triangle, grid: &mut Grid<char>, depth_buffer: &mut Gri
             grid.set(value, x as usize, y as usize);
         }
     }
-}   
+}
 
 fn main() {
     let mut grid = Grid::new(' ', WIDTH, HEIGHT);
@@ -172,8 +185,7 @@ fn main() {
         dz: 0.0,
 
         position: Vector3::new(0.2, 0.0, 1.0),
-        scale: 1.0,
-        value: '.'
+        scale: 0.25,
     };
 
     let mut transform2 = Transform {
@@ -187,7 +199,6 @@ fn main() {
 
         position: Vector3::new(0.6, -0.5, 2.0),
         scale: 0.5,
-        value: '#',
     };
 
     let mut transform3 = Transform {
@@ -199,9 +210,8 @@ fn main() {
         dy: 1.0,
         dz: 3.0,
 
-        position: Vector3::new(0, 1.0, 0),
-        scale: 0.5,
-        value: '/'
+        position: Vector3::new(0, 0.0, 0.0),
+        scale: 0.75,
     };
 
     let mut triangles = vec![(tri1, transform1), (tri2, transform2), (tri3, transform3)];
@@ -210,9 +220,9 @@ fn main() {
     let mut camera_pitch = 0.0;
     let mut camera_yaw = 0.0;
     let mut camera_roll = 0.0;
-    
+
     // In world coordinates
-    let light = Vector3::new(0.0, 2.0, 0.0);
+    let light = Vector3::new(0.5, -3.0, 0.5);
 
     // Perspective matrix
     let fov = Angle::Degrees(90.0);
@@ -314,7 +324,6 @@ fn main() {
                 dz,
                 position,
                 scale,
-                value
             } = *transform;
 
             // Scaling matrix
@@ -335,20 +344,12 @@ fn main() {
             let ac = c - a;
             let normal = ab.cross(ac).normalize();
 
-            //Calculating normal matrix
-            let model_inverse = Matrix4::scale(1.0/scale) * rotation.transpose() * Matrix4::translation(-position);
+            //Calculating world normal matrix
+            let model_inverse = Matrix3::scale(1.0 / scale) * rotation.cartesian().transpose();
             let normal_matrix = model_inverse.transpose();
-                
-            // Converting normal to world space
-            let normal = (normal_matrix * Vector4::to_vector4(normal, 0.0)).xyz().normalize();
-            
-            // Calculate light value
-            let l = (light - normal).normalize();
-            let value = f32::max(0.0, normal.dot(l));
-            println!("{value}");
-            let value = f32::round(value * 3.0) as usize;
 
-            let character: char = "./$#".as_bytes()[value] as char;
+            // Converting normal vector to world space
+            let normal = (normal_matrix * normal).normalize();
 
             // Transform points using matrices
             let a = perspective * view * translation * rotation * scalar * a;
@@ -362,7 +363,7 @@ fn main() {
 
             // Final projected triangle
             let t = Triangle { a, b, c };
-            rasterize_triangle(t, &mut grid, &mut depth_buffer, character);
+            rasterize_triangle(t, &mut grid, &mut depth_buffer, normal, light);
 
             transform.yaw += transform.dx;
             transform.pitch += transform.dy;
@@ -374,5 +375,4 @@ fn main() {
         grid.clear(' ');
         depth_buffer.clear(f32::INFINITY);
     }
-
 }
